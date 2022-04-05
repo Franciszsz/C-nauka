8106141375c7ff28b5b5484cf92baf2897f342a6
Franciszsz <100140336+Franciszsz@users.noreply.github.com>
2022-04-05 09:54:53 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,3 +15,40 @@ private:
 	double waga;
 };
 
+//pomocy.h
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class pies
+{
+public:
+	pies();
+	pies(string imie, double waga);
+	void daj_glos();
+private:
+	string imie;
+	double waga;
+};
+//pomocy.cpp
+
+#include "pomocy.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+pies::pies() :imie("Nieznany"), waga(0)
+{
+
+}
+pies::pies(string aimie, double awaga) : imie( aimie), waga(awaga)
+{
+
+}
+void pies::daj_glos()
+{
+	cout << imie << ", waga = " << waga << endl;
+}
